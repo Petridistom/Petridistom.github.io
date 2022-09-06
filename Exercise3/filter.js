2c3c5b4df938583775bbaadee2ef1ff2b672b5f1
Petridistom <110432739+Petridistom@users.noreply.github.com>
2022-09-06 15:16:36 +1000

    Add files via upload

--- Exercise3/filter.js
+++ Exercise3/filter.js
@@ -1,3 +1,8 @@
+
+// checks the data tag (filter selection) and determines if the recipe is passes the filter (filter by).
+// when calling this function, by clicking on a category and then a filter, the website will 
+//only display recipes that have tags that pass the filter
+
 function filterCategory(filterSelection, filterBy){
 let recipeCards = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
 // console.log(recipeCards);
@@ -12,9 +17,11 @@ for(let listItem of recipeCards){
 }
 }
 
+
+//removes all filters
 function allCategories(){
     let recipeCards = Array.from(document.getElementsByClassName("RecipeCardWrapper"));
-    // console.log(recipeCards);
+    // console.log(recipeCards); 
     
     for(let listItem of recipeCards){
         // console.log(listItem.dataset.author)
