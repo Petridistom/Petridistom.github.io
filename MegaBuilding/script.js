508b3e9dfa3e17fbc87ca3cf539b866eb0f92556
Petridistom <110432739+Petridistom@users.noreply.github.com>
2022-10-24 03:38:41 +1100

    Add files via upload

---
+++ MegaBuilding/script.js
@@ -0,0 +1,18 @@
+//swap imagess on hover
+/* window.onload = function (){
+
+
+
+let Intimidation = document.getElementById('Intimdation');
+let Symmetry = document.getElementById('Symmetry');
+let Brutalism = document.getElementById('Brutalism');
+
+Intimidation.addEventListener('mouseover', swapInt);
+
+function swapInt(){
+    document.getElementById('Intimidation').src = url('images/1x/intimidation-swap.png')
+    console.log('hovering')
+}
+
+
+} */
